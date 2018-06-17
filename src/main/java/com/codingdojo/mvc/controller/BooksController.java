392704cbb8361310028fdb21484310f3b19aaca2
Aria Samandi <ariasamandi@gmail.com>
2018-06-17 16:01:19 -0700

    commit

--- src/main/java/com/codingdojo/mvc/controller/BooksController.java
+++ src/main/java/com/codingdojo/mvc/controller/BooksController.java
@@ -8,6 +8,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
@@ -27,19 +28,42 @@ public class BooksController {
  public String index(Model model) {
      List<Book> books = bookService.allBooks();
      model.addAttribute("books", books);
-     return "/books/index.jsp";
+     return "books/index.jsp";
  }
  @RequestMapping("/books/new")
  public String newBook(@ModelAttribute("book") Book book) {
-     return "/books/new.jsp";
+     return "books/new.jsp";
  }
  @RequestMapping(value="/books", method=RequestMethod.POST)
  public String create(@Valid @ModelAttribute("book") Book book, BindingResult result) {
      if (result.hasErrors()) {
-         return "/books/new.jsp";
+         return "books/new.jsp";
      } else {
          bookService.createBook(book);
          return "redirect:/books";
      }
  }
+ @RequestMapping("/books/{num}")
+ public String show(@PathVariable(value="num", required=true) Long num, Model model) {
+	 Book book = bookService.findBook(num);
+	 model.addAttribute("book", book);
+	 return "books/show.jsp";
+ }
+  @RequestMapping("/books/{id}/edit")
+  public String edit(@PathVariable("id") Long id, Model model) {
+      Book book = bookService.findBook(id);
+      model.addAttribute("book", book);
+      return "/books/edit.jsp";
+  }
+  
+  @RequestMapping(value="/books/{id}", method=RequestMethod.PUT)
+  public String update(@Valid @ModelAttribute("book") Book book, BindingResult result) {
+      if (result.hasErrors()) {
+          return "/books/edit.jsp";
+      } else {
+          bookService.updateBook(book);
+          return "redirect:/books";
+      }
+  }
+}
 }
